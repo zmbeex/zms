8ef0f56e7ef40f6aab0d5f818f39452648cb23c4
明 <oncwnuClhtj35bo9ojs7ggQ11Sag@git.weixin.qq.com>
2020-08-20 13:36:49 +0800

    init

---
+++ client.go
@@ -0,0 +1,174 @@
+package zmsClient
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/zmbeex/gkit"
+	"net"
+	"os"
+	"reflect"
+	"runtime/debug"
+	"strings"
+	"time"
+)
+
+type ZmsClient struct {
+	Conn        *websocket.Conn
+	Url         string   `title:"ws的url"`
+	Reconnect   chan int `title:"重新连接信道"`
+	isReconnect bool     //正在重新连接
+}
+
+func (c *ZmsClient) reconnect() {
+	if c.isReconnect {
+		return
+	}
+	gkit.Debug("开始连接注册中心")
+	c.isReconnect = true
+	defer func() {
+		c.isReconnect = false
+	}()
+
+	u := new(Zurl)
+	timeStamp := time.Now().Unix()
+	signString := "zms.test|" + gkit.ToString(timeStamp) + "|" + Cache.Uuid
+	signString += "|" + Cache.Set.UserName + "|" + Cache.Set.Password
+	u.Add(Cache.Set.GatewayHost + "?")
+	u.Push("timeStamp", timeStamp)
+	u.Push("uid", Cache.Uuid)
+	u.Push("sign", gkit.GetSHA(signString))
+	u.Push("userName", Cache.Set.UserName)
+	u.Push("servers", gkit.SetAesCBC(gkit.SetJson(Cache.ServerInfo), Cache.Set.ServerInfoKey))
+	c.Url = u.String()
+	conn, _, err := websocket.DefaultDialer.Dial(c.Url, nil)
+
+	gkit.Error(signString)
+	gkit.Error(gkit.GetSHA(signString))
+	if err != nil {
+		gkit.Error("连接失败:" + err.Error())
+		time.Sleep(5 * time.Second)
+		c.reconnect()
+		return
+	}
+	gkit.Info("连接注册中心成功")
+	c.Conn = conn
+}
+
+// 处理心跳
+func (c *ZmsClient) Hearbeat() {
+	for {
+		if c.Conn == nil {
+			time.Sleep(1 * time.Second)
+			c.reconnect()
+			continue
+		}
+		time.Sleep(time.Second * 10)
+		result := new(zms.Result)
+		result.Code = "zms.system.heart.beats"
+		result.Uuid = Cache.Uuid
+		result.Status = 1
+		c.Write(result)
+	}
+}
+
+//处理websocket消息
+func (c *ZmsClient) handleMessage() {
+	for {
+		if c.Conn == nil {
+			time.Sleep(1 * time.Second)
+		} else {
+			break
+		}
+	}
+	for {
+		_, message, err := (*c.Conn).ReadMessage()
+		if err != nil {
+			if _, ok := err.(*net.OpError); ok {
+				gkit.Warn("网络错误,连接中断")
+				time.Sleep(10 * time.Second)
+				c.reconnect()
+				continue
+			}
+			if _, ok := err.(*websocket.CloseError); ok {
+				fmt.Println("服务端关闭了连接")
+				time.Sleep(10 * time.Second)
+				c.reconnect()
+				continue
+			}
+
+			fmt.Println("错误类型", reflect.TypeOf(err))
+			gkit.Debug("错误：" + err.Error())
+			os.Exit(1)
+			return
+		}
+		go c.dealMessage(string(message))
+	}
+}
+
+// 处理结果
+func (c *ZmsClient) dealMessage(msg string) {
+	gkit.Debug("接收到信息：" + msg)
+	params := new(zms.Params)
+	result := new(zms.Result)
+	defer func() {
+		r := recover()
+		if r != nil {
+			gkit.Info("报文异常===========================")
+			gkit.Error(string(debug.Stack()))
+		}
+		c.Write(result)
+	}()
+	err := gkit.GetJson(msg, params)
+	if err != nil {
+		gkit.Error("报文数据异常：" + params.Code)
+		result.Status = -1
+		return
+	}
+	params.Code = strings.TrimPrefix(params.Code, Cache.Set.UserName+".")
+	result.Code = params.Code
+	result.Uuid = params.Uuid
+	r := Cache.ServerRouterMap[params.Code]
+	if r == nil {
+		result.Status = -2
+		gkit.Error("收到无效服务：" + params.Code)
+		return
+	}
+	z := zms.InitZms(params, result)
+	defer func() {
+		r := recover()
+		if r == nil {
+			return
+		}
+		err, ok := r.(error)
+		if ok {
+			z.SetStatus(-2)
+			z.SetNote(err.Error())
+			return
+		}
+		n, ok := r.(int)
+		if ok {
+			z.SetStatus(n * -1)
+			return
+		}
+		note, ok := r.(string)
+		if ok {
+			z.SetStatus(-3)
+			z.SetNote(note)
+			return
+		}
+		z.SetStatus(-4)
+		z.SetNote("未知异常")
+	}()
+	r.Func(z)
+	result.Status = 1
+}
+
+// 写入数据
+func (c *ZmsClient) Write(msg *zms.Result) {
+	if msg.Status == 0 {
+		msg.Status = 1
+	}
+	gkit.Debug("发送报文：" + gkit.SetJson(msg))
+	err := c.Conn.WriteJSON(msg)
+	gkit.CheckError(err, "数据发送失败")
+}
---
+++ route.go
@@ -0,0 +1,61 @@
+package zmsClient
+
+import (
+	"github.com/zmbeex/gkit"
+	"strings"
+)
+
+type ZmsRouter struct {
+	Code   string      `title:"服务编码"`
+	Desc   string      `title:"服务描述"`
+	Role   []string    `title:"访问角色"`
+	Params interface{} `title:"参数模型"`
+	Func   func(z *zms.Zms)
+}
+
+func (r ZmsRouter) Register() {
+	Cache.ServerRouterMap[r.Code] = &r
+	info := new(ServerInfo)
+	info.Code = r.Code
+	info.Desc = r.Desc
+	info.Role = r.Role
+	info.Params = map[string]interface{}{
+		"Params": r.Params,
+	}
+	Cache.ServerInfo[r.Code] = info
+	gkit.Info("注册路由：" + info.Code + " -> " + info.Desc + " -> " + strings.Join(info.Role, ","))
+}
+
+type ServerInfo struct {
+	Code   string                 `title:"服务编码"`
+	Desc   string                 `title:"服务描述"`
+	Role   []string               `title:"访问角色"`
+	Params map[string]interface{} `title:"参数模型"`
+}
+
+type Zurl struct {
+	val string
+}
+
+func (z *Zurl) Add(val string) {
+	z.val += val
+}
+
+func (z *Zurl) Push(key string, value interface{}) {
+	z.val += "&" + key + "=" + gkit.ToString(value)
+}
+
+func (z *Zurl) String() string {
+	return z.val
+}
+
+func Run() {
+	c := new(ZmsClient)
+
+	// 重新连接
+	c.reconnect()
+	// 处理消息
+	go c.handleMessage()
+	// 发送心跳包
+	c.Hearbeat()
+}
