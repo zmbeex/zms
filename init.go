9d6303cd90aec640a833f2e0d56200a91bc35960
周明 <zmbeex@outlook.com>
2020-08-22 19:32:48 +0800

    配置

--- init.go
+++ init.go
@@ -1,6 +1,9 @@
 package zms
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+	"github.com/zmbeex/gkit"
+)
 
 // 参数
 type Params struct {
@@ -28,22 +31,22 @@ var Cache struct {
 }
 
 type Setting struct {
-	UserName      string `title:"账号"`
-	Password      string `title:"密码"`
-	GatewayHost   string `title:"网关"`
-	ServerInfoKey string `title:"服务信息加密密钥"`
+	UserName      string `title:"账号" defaultValue:"dev"`
+	Password      string `title:"密码" defaultValue:"qwertyuiop3466f"`
+	GatewayHost   string `title:"网关" defaultValue:"ws://localhost:8088/gateway"`
+	ServerInfoKey string `title:"服务信息加密密钥" defaultValue:"1234567812345678"`
 }
 
 func init() {
 	set := new(Setting)
-	set.UserName = "dev"
-	set.Password = "qwertyuiop3466f"
-	set.GatewayHost = "wss://zmbeex.com/gateway/nest"
-	//set.GatewayHost = "ws://192.168.0.108:8088/nest"
-	set.ServerInfoKey = "1234567812345678"
-	Cache.Set = set
-
-	Cache.ServerRouterMap = make(map[string]*Router)
-	Cache.Uuid = uuid.NewV4().String()
-	Cache.ServerInfo = make(map[string]*ServerInfo)
+	//set.UserName = "dev"
+	//set.Password = "qwertyuiop3466f"
+	//set.GatewayHost = "wss://zmbeex.com/gateway/nest"
+	//set.ServerInfoKey = "1234567812345678"
+	gkit.InitSetting("zms", set, "zms客户端", func() {
+		Cache.Set = set
+		Cache.ServerRouterMap = make(map[string]*Router)
+		Cache.Uuid = uuid.NewV4().String()
+		Cache.ServerInfo = make(map[string]*ServerInfo)
+	})
 }
